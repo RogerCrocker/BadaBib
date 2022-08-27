ec028b8af542e5ca6b300d69b5a78533f2387b05
Roger Crocker <roger.crocker@pm.me>
2022-08-27 16:26:15 -0400

    More detailed comments in config_manager.py

--- src/config_manager.py
+++ src/config_manager.py
@@ -19,9 +19,11 @@ from gi.repository import Gio, GLib
 from .default_layouts import default_layout_strings
 
 
+# Application settings
 setting = Gio.Settings.new("com.github.rogercrocker.badabib")
 
 
+# Dict of entry types and their display names
 entrytype_dict = {
     "article": "Article",
     "book": "Book",
@@ -41,6 +43,7 @@ entrytype_dict = {
 }
 
 
+# Dict of fields and their display names
 field_dict = {
     "ENTRYTYPE": "Entry Type",
     "ID": "BibTeX Key",
@@ -76,6 +79,7 @@ field_dict = {
 }
 
 
+# Dict of month macros
 month_dict = {
     "jan": "January",
     "feb": "February",
@@ -92,23 +96,79 @@ month_dict = {
 }
 
 
+# Fields by which the itemlist can be sorted
 sort_fields = ["ID", "author", "title", "journal", "year"]
+
+# Fields for which a "link icon" is displayed in the itemlist
 link_fields = ["doi", "url", "eprint", "file"]
 
 
-def get_color_scheme():
-    return setting.get_int("color-scheme")
+def get_editor_layout(entrytype):
+    """
+    Returns a string that decribes the editor layout for the given entrytype.
+
+    Parameters
+    ----------
+    entrytype: str
+
+    Returns
+    -------
+    str
+        layout string
+    """
+    # Return default layout of default entrytype for unknown entrytypes
+    if entrytype not in entrytype_dict:
+        return default_layout_strings[get_default_entrytype()]
+
+    # If defined, return custom layout
+    layouts = setting.get_value("editor-layouts")
+    if layouts:
+        idx = list(entrytype_dict.keys()).index(entrytype)
+        if layouts[idx]:
+            return layouts[idx]
+
+    # Otherwise, return default layout
+    return default_layout_strings[entrytype]
 
 
-def set_color_scheme(n):
-    setting.set_int("color-scheme", n)
+def set_editor_layout(entrytype, layout):
+    """
+    Set editor layout for given entry type.
 
+    Parameters
+    ----------
+    entrytype: str
+    layout: str
+    """
+    # Get custom layouts
+    layouts = list(setting.get_value("editor-layouts"))
+
+    # Check if custom layouts are defined
+    if len(layouts) != len(entrytype_dict):
+        layouts = len(entrytype_dict) * [""]
+
+    # Ignore unknown entry types
+    if entrytype in entrytype_dict:
+        idx = list(entrytype_dict.keys()).index(entrytype)
+        # Reset custom layout if it is identical to default
+        if layout == default_layout_strings[entrytype]:
+            layouts[idx] = ""
+        else:
+            layouts[idx] = layout
+        # Set new layout
+        g_variant = GLib.Variant("as", layouts)
+        setting.set_value("editor-layouts", g_variant)
+
+
+# Getter and setter for unified access to settings.
+# See gschema.xml for details.
 
 def get_align_fields():
     return setting.get_boolean("align-fields")
 
 
 def set_align_fields(state):
+    """state: bool"""
     setting.set_boolean("align-fields", state)
 
 
@@ -117,6 +177,7 @@ def get_field_indent():
 
 
 def set_field_indent(n):
+    """n: int"""
     setting.set_int("field-indent", n)
 
 
@@ -133,6 +194,7 @@ def get_parse_on_fly():
 
 
 def set_parse_on_fly(state):
+    """state: bool"""
     setting.set_boolean("parse-on-fly", state)
 
 
@@ -141,6 +203,7 @@ def get_create_backup():
 
 
 def set_create_backup(state):
+    """state: bool"""
     setting.set_boolean("create-backup", state)
 
 
@@ -149,6 +212,7 @@ def get_remember_strings():
 
 
 def set_remember_strings(state):
+    """state: bool"""
     setting.set_boolean("remember-strings", state)
 
 
@@ -157,6 +221,7 @@ def get_highlight_syntax():
 
 
 def set_highlight_syntax(state):
+    """state: bool"""
     setting.set_boolean("highlight-syntax", state)
 
 
@@ -164,8 +229,9 @@ def get_window_geom():
     return setting.get_value("window-geom")
 
 
-def set_window_geom(lis):
-    g_variant = GLib.Variant("ai", lis)
+def set_window_geom(geom):
+    """geom: list of int"""
+    g_variant = GLib.Variant("ai", geom)
     setting.set_value("window-geom", g_variant)
 
 
@@ -174,6 +240,7 @@ def get_undo_delay():
 
 
 def set_undo_delay(d):
+    """d: float"""
     setting.set_double("undo-delay", d)
 
 
@@ -181,8 +248,9 @@ def get_row_indent():
     return setting.get_int("row-indent")
 
 
-def set_row_indent(indent):
-    setting.set_int("row-indent", indent)
+def set_row_indent(n):
+    """n: int"""
+    setting.set_int("row-indent", n)
 
 
 def get_default_entrytype():
@@ -198,6 +266,7 @@ def get_new_file_name():
 
 
 def set_new_file_name(name):
+    """name: str"""
     setting.set_string("new-file-name", name)
 
 
@@ -219,6 +288,7 @@ def get_open_tab():
 
 
 def set_open_tab(filename):
+    """filename: str"""
     setting.set_string("open-tab", filename)
 
 
@@ -227,6 +297,7 @@ def get_num_recent():
 
 
 def set_num_recent(n):
+    """n: int"""
     setting.set_int("num-recent", n)
 
 
@@ -237,6 +308,7 @@ def get_recent_files():
 
 
 def set_recent_files(recent_files):
+    """recent_files: dict, {file name: file state}"""
     g_variant_files = GLib.Variant("as", list(recent_files.keys()))
     g_variant_states = GLib.Variant("as", list(recent_files.values()))
     setting.set_value("recent-files", g_variant_files)
@@ -244,22 +316,29 @@ def set_recent_files(recent_files):
 
 
 def add_to_recent(badabib_file):
+    """badabib_file: BadaBibFile"""
     recent_files = get_recent_files()
+    # If file already in recent, remove and re-add at top of list
     if badabib_file.name in recent_files:
         recent_files.pop(badabib_file.name)
     recent_files[badabib_file.name] = badabib_file.itemlist.state_to_string()
 
+    # Truncate list to set number of files
     if len(recent_files) > get_num_recent():
         first_file = list(recent_files.keys())[0]
         recent_files.pop(first_file)
 
+    # Set recent files
     set_recent_files(recent_files)
 
 
 def remove_from_recent(filename):
+    """filename: str"""
     recent_files = get_recent_files()
+    # Remove file
     if filename in recent_files:
         recent_files.pop(filename)
+    # Update list of recent files
     set_recent_files(recent_files)
 
 
@@ -268,33 +347,9 @@ def get_string_imports():
 
 
 def set_string_imports(string_files):
+    """
+    string_files: dict, {file name: None}
+        file name dict with empty state
+    """
     g_variant_files = GLib.Variant("as", list(string_files.keys()))
     setting.set_value("string-imports", g_variant_files)
-
-
-def get_editor_layout(entrytype):
-    if entrytype not in entrytype_dict:
-        return default_layout_strings[get_default_entrytype()]
-
-    layouts = setting.get_value("editor-layouts")
-    if layouts:
-        idx = list(entrytype_dict.keys()).index(entrytype)
-        if layouts[idx]:
-            return layouts[idx]
-
-    return default_layout_strings[entrytype]
-
-
-def set_editor_layout(entrytype, layout):
-    layouts = list(setting.get_value("editor-layouts"))
-    if len(layouts) != len(entrytype_dict):
-        layouts = len(entrytype_dict) * [""]
-
-    if entrytype in entrytype_dict:
-        idx = list(entrytype_dict.keys()).index(entrytype)
-        if layout == default_layout_strings[entrytype]:
-            layouts[idx] = ""
-        else:
-            layouts[idx] = layout
-        g_variant = GLib.Variant("as", layouts)
-        setting.set_value("editor-layouts", g_variant)
